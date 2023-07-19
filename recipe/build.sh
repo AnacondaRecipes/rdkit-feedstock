b8fc10bdb6cf91861a4dfab8e1586a3b8dd874d1
Serhii Kupriienko <skupriienko@anaconda.com>
2023-07-19 10:58:55 +0300

    rdkit 2023.03.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,13 @@
 
 set -euxo pipefail
 
+echo "Building ${PKG_NAME}."
+
+
+# Isolate the build.
+mkdir -p Build-${PKG_NAME}
+cd Build-${PKG_NAME} || exit 1
+
 POPCNT_OPTIMIZATION="ON"
 if [[ "$target_platform" == linux-ppc64le || "$target_platform" == linux-aarch64 ]]; then
     # PowerPC includes -mcpu=power8 optimizations already
@@ -15,33 +22,48 @@ if [[ "$target_platform" == linux-ppc64le ]]; then
     EXTRA_CMAKE_FLAGS+=" -D PYTHON_NUMPY_INCLUDE_PATH=${SP_DIR}/numpy/core/include"
 fi
 
-cmake ${CMAKE_ARGS} \
-    -D CMAKE_BUILD_TYPE=Release \
-    -D CMAKE_INSTALL_PREFIX="$PREFIX" \
-    -D BOOST_ROOT="$PREFIX" \
-    -D Boost_NO_SYSTEM_PATHS=ON \
-    -D Boost_NO_BOOST_CMAKE=ON \
-    -D PYTHON_EXECUTABLE="$PYTHON" \
-    -D PYTHON_INSTDIR="$SP_DIR" \
-    -D RDK_BUILD_AVALON_SUPPORT=ON \
-    -D RDK_BUILD_CAIRO_SUPPORT=ON \
-    -D RDK_BUILD_CPP_TESTS=OFF \
-    -D RDK_BUILD_INCHI_SUPPORT=ON \
-    -D RDK_BUILD_FREESASA_SUPPORT=ON \
-    -D RDK_BUILD_YAEHMOP_SUPPORT=ON \
-    -D RDK_BUILD_XYZ2MOL_SUPPORT=ON \
-    -D RDK_BUILD_PYTHON_WRAPPERS=ON \
-    -D RDK_INSTALL_INTREE=OFF \
-    -D RDK_INSTALL_STATIC_LIBS=OFF \
-    -D RDK_OPTIMIZE_POPCNT=${POPCNT_OPTIMIZATION} \
-    ${EXTRA_CMAKE_FLAGS} \
-    .
+# Generate the build files.
+echo "Generating the build files..."
 
-make -j$CPU_COUNT
-make install
+cmake .. ${CMAKE_ARGS} \
+-GNinja \
+-D CMAKE_PREFIX_PATH=${PREFIX} \
+-D CMAKE_INSTALL_PREFIX=${PREFIX} \
+-D CMAKE_BUILD_TYPE=Release \
+-D BOOST_ROOT="$PREFIX" \
+-D Boost_NO_SYSTEM_PATHS=ON \
+-D Boost_NO_BOOST_CMAKE=ON \
+-D PYTHON_EXECUTABLE="$PYTHON" \
+-D PYTHON_INSTDIR="$SP_DIR" \
+-D RDK_BUILD_AVALON_SUPPORT=ON \
+-D RDK_BUILD_CAIRO_SUPPORT=ON \
+-D RDK_BUILD_CPP_TESTS=OFF \
+-D RDK_BUILD_INCHI_SUPPORT=ON \
+-D RDK_BUILD_FREESASA_SUPPORT=ON \
+-D RDK_BUILD_YAEHMOP_SUPPORT=ON \
+-D RDK_BUILD_XYZ2MOL_SUPPORT=ON \
+-D RDK_BUILD_PYTHON_WRAPPERS=ON \
+-D RDK_INSTALL_INTREE=OFF \
+-D RDK_INSTALL_STATIC_LIBS=OFF \
+-D RDK_OPTIMIZE_POPCNT=${POPCNT_OPTIMIZATION} \
+${EXTRA_CMAKE_FLAGS}
+
+
+# Build.
+echo "Building..."
+ninja -j${CPU_COUNT} || exit 1
 
 ## How to run unit tests:
 ## 1. Set RDK_BUILD_CPP_TESTS to ON
 ## 2. Uncomment lines below
 # export RDBASE="$SRC_DIR"
 # ctest --output-on-failure
+
+# Installing
+echo "Installing..."
+ninja install || exit 1
+
+
+# Error free exit!
+echo "Error free exit!"
+exit 0
